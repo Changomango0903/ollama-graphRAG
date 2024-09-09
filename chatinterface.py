2adeb68a7c114b4fc51faca41ab45de0167249c9
Sean <schang09032003@gmail.com>
2024-09-09 17:52:13 +0800

    doc: Marked where processes for RAG config will go

--- chatinterface.py
+++ chatinterface.py
@@ -6,11 +6,13 @@ import shutil
 files = []
 
 def random_response(message, history):
+    #Chat here
     return random.choice(["Yes", "No"])
 
 def process_file(fileobj):
     path = "./docs/" + os.path.basename(fileobj)
     shutil.copyfile(fileobj.name, path)
+    #File processing here
 
 with gr.Blocks() as demo:
     chatInterface = gr.ChatInterface(random_response)
@@ -21,3 +23,7 @@ with gr.Blocks() as demo:
         ]
     )
 demo.launch(server_name='0.0.0.0')
+
+#Control Flow for GraphRAG LLM:
+# Load into Gradio -> Detect File Insertion -> Generate Query
+# -> Initialize LLM with Neo4j -> Ollama with GraphRAG into random_response
